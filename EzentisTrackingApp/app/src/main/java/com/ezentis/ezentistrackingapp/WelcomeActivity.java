ae2cea3e213d7ba1dec864895509b7d133892992
Javier Gamarra <nhpatt@gmail.com>
2016-06-16 14:40:59 +0200

    simplify code

--- EzentisTrackingApp/app/src/main/java/com/ezentis/ezentistrackingapp/WelcomeActivity.java
+++ EzentisTrackingApp/app/src/main/java/com/ezentis/ezentistrackingapp/WelcomeActivity.java
@@ -18,14 +18,7 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
 
     @Override
     public void onClick(View v) {
-
-        Intent intent;
-        if (v.getId() == R.id.register) {
-            intent = new Intent(this, RegisterActivity.class);
-        } else {
-            intent = new Intent(this, LoginActivity.class);
-        }
-
+        Intent intent = new Intent(this, v.getId() == R.id.register ? RegisterActivity.class : LoginActivity.class);
         startActivity(intent);
     }
 }
