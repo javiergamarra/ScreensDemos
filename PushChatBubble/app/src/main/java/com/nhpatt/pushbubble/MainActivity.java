b2145249fef48919cdd0f332ac0a91f939a5d021
Javier Gamarra <nhpatt@gmail.com>
2016-06-16 10:50:14 +0200

    update push app to 1.4.0

--- PushChatBubble/app/src/main/java/com/nhpatt/pushbubble/MainActivity.java
+++ PushChatBubble/app/src/main/java/com/nhpatt/pushbubble/MainActivity.java
@@ -14,79 +14,79 @@ import com.liferay.mobile.android.service.Session;
 import com.liferay.mobile.android.service.SessionImpl;
 import com.liferay.mobile.push.Push;
 import com.liferay.mobile.screens.push.PushScreensActivity;
+import com.liferay.mobile.screens.util.LiferayLogger;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends PushScreensActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
-	@Override
-	protected Session getDefaultSession() {
-		String server = getString(R.string.server);
-		String email = getString(R.string.user);
-		String password = getString(R.string.password);
-		return new SessionImpl(server, new BasicAuthentication(email, password));
-	}
+    @Override
+    protected Session getDefaultSession() {
+        String server = getString(R.string.server);
+        String email = getString(R.string.user);
+        String password = getString(R.string.password);
+        return new SessionImpl(server, new BasicAuthentication(email, password));
+    }
 
-	@Override
-	protected void onPushNotificationReceived(final JSONObject jsonObject) {
-		updateLinearOnUiThread(jsonObject, R.id.linear);
-	}
+    @Override
+    protected void onPushNotificationReceived(final JSONObject jsonObject) {
+        updateLinearOnUiThread(jsonObject, R.id.linear);
+    }
 
-	@Override
-	protected void onErrorRegisteringPush(final String s, final Exception e) {
+    @Override
+    protected void onErrorRegisteringPush(final String message, final Exception e) {
+        LiferayLogger.e(message, e);
+    }
 
-	}
+    @Override
+    protected String getSenderId() {
+        return getString(R.string.sender_id);
+    }
 
-	@Override
-	protected String getSenderId() {
-		return getString(R.string.sender_id);
-	}
+    private void updateLinearOnUiThread(final JSONObject jsonObject, final int id) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                LinearLayout linear = (LinearLayout) findViewById(id);
+                linear.addView(createTextView(jsonObject));
+            }
+        });
+    }
 
-	private void updateLinearOnUiThread(final JSONObject jsonObject, final int id) {
-		runOnUiThread(new Runnable() {
-			@Override
-			public void run() {
-				LinearLayout linear = (LinearLayout) findViewById(id);
-				linear.addView(createTextView(jsonObject));
-			}
-		});
-	}
+    private TextView createTextView(final JSONObject rootObject) {
+        try {
+            String body = rootObject.getString("body");
+            JSONObject jsonObject = new JSONObject(body);
+            boolean brian = "brian".equals(jsonObject.getString("name"));
+            int leftMargin = brian ? 100 : 0;
+            int rightMargin = brian ? 0 : 100;
+            int color = brian ? android.R.color.holo_red_dark : android.R.color.holo_green_dark;
+            String message = jsonObject.getString("message");
 
-	private TextView createTextView(final JSONObject rootObject) {
-		try {
-			String body = rootObject.getString("body");
-			JSONObject jsonObject = new JSONObject(body);
-			boolean brian = "brian".equals(jsonObject.getString("name"));
-			int leftMargin = brian ? 100 : 0;
-			int rightMargin = brian ? 0 : 100;
-			int color = brian ? android.R.color.holo_red_dark : android.R.color.holo_green_dark;
-			String message = jsonObject.getString("message");
+            return createTextView(message, leftMargin, rightMargin, getResources().getColor(color));
+        } catch (JSONException e) {
+            Log.e("TAG", "Error parsing message");
+        }
+        return null;
+    }
 
-			return createTextView(message, leftMargin, rightMargin, getResources().getColor(color));
-		}
-		catch (JSONException e) {
-			Log.e("TAG", "Error parsing message");
-		}
-		return null;
-	}
-
-	@NonNull
-	private TextView createTextView(final String message, final int leftMargin, final int rightMargin, final int color) {
-		TextView textView = new TextView(this);
-		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-		params.setMargins(leftMargin, 10, rightMargin, 10);
-		textView.setLayoutParams(params);
-		textView.setText(message);
-		textView.setBackgroundColor(color);
-		textView.setPadding(10, 10, 10, 10);
-		return textView;
-	}
+    @NonNull
+    private TextView createTextView(final String message, final int leftMargin, final int rightMargin, final int color) {
+        TextView textView = new TextView(this);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.setMargins(leftMargin, 10, rightMargin, 10);
+        textView.setLayoutParams(params);
+        textView.setText(message);
+        textView.setBackgroundColor(color);
+        textView.setPadding(10, 10, 10, 10);
+        return textView;
+    }
 
 }
